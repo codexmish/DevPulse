7dd6d5ee629c26b26c6947212ff5f13b348daad7
codexmish <majharulislam.shanto.70@gmail.com>
2026-05-22 10:18:10 +0600

    server running

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { type Request, type Response } from "express"
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(8000, () => {
+  console.log("server is running")
+})
